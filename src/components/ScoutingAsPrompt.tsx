99c8c3a07a75dad1bb3e6e415a9008bd382c69e2
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-06-20 20:39:05 +0200

    animate loading spinner and empty player list when resubmitting query

--- src/components/ScoutingAsPrompt.tsx
+++ src/components/ScoutingAsPrompt.tsx
@@ -24,6 +24,7 @@ export default function Chat() {
     baseURL: BASE_URL
   })
   const [playerList, setPlayerList] = useState<Player[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
 
   const formSchema = z.object({
@@ -85,12 +86,13 @@ export default function Chat() {
   }
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log("starting request against backend")
+    setPlayerList([])
+    setIsLoading(true)
     const query = values.prompt
     const response = await api.post("scout-prompt", { query })
-    console.log(response)
 
     let convertedPlayers = await convertToPlayerList(response.data.response.list)
+    setIsLoading(false)
     setPlayerList(convertedPlayers)
   }
 
@@ -111,7 +113,15 @@ export default function Chat() {
           )}
         />
         {<PlayerList playerList={playerList}></PlayerList>}
-        <Button type="submit">Find next Messi now!</Button>
+        {!isLoading && <Button type="submit" >Submit</Button>}
+        {isLoading && <Button type="button" disabled>
+          <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          Processing...
+        </Button>
+        }
       </form>
     </Form>
   )
