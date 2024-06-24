6be4eb4bd0b9f16d8d7c01a24c0d8d373c9a482d
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-06-24 13:53:35 +0200

    add skeleton on loading of backend

--- src/components/ScoutingAsPrompt.tsx
+++ src/components/ScoutingAsPrompt.tsx
@@ -5,9 +5,12 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import PlayerList, { Player } from "./PlayerList"
 import { Button } from "./ui/button"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
 import { BASE_URL } from "@/config/constants"
 import axios from 'axios'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
+import { Link } from "react-router-dom"
+import Skeleton from "./Skeleton"
 
 export type PlayerAPIResponse = {
   player_id: number;
@@ -28,13 +31,15 @@ export default function Chat() {
 
 
   const formSchema = z.object({
-    prompt: z.string().min(2).max(1000)
+    prompt: z.string().min(2).max(1000),
+    position: z.string().min(0).max(1000)
   })
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       prompt: "",
+      position: "",
     },
   })
 
@@ -101,6 +106,32 @@ export default function Chat() {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
+          name="position"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Position</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a position to search for..." />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="goalkeeper">Goalkeeper</SelectItem>
+                  <SelectItem value="centerback">Center Back</SelectItem>
+                  <SelectItem value="leftcenterback">Left Center Back</SelectItem>
+                  <SelectItem value="rightcenterback">Right Center Back</SelectItem>
+                  <SelectItem value="leftback">Leftback</SelectItem>
+                  <SelectItem value="rightwing">Right winger</SelectItem>
+                  <SelectItem value="leftwing">Left winger</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
           name="prompt"
           render={({ field }) => (
             <FormItem>
@@ -112,15 +143,20 @@ export default function Chat() {
             </FormItem>
           )}
         />
+
         {<PlayerList playerList={playerList}></PlayerList>}
         {!isLoading && <Button type="submit" >Submit</Button>}
-        {isLoading && <Button type="button" disabled>
-          <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
-          Processing...
-        </Button>
+        {isLoading &&
+          <>
+            <Skeleton></Skeleton>
+            <Button type="button" disabled>
+              <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+              Processing...
+            </Button>
+          </>
         }
       </form>
     </Form>
---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function Skeleton() {
+    return (
+        <div className='space-y-4'>
+            <div className="border  shadow rounded-md p-4 max-w-sm w-full">
+                <div className="animate-pulse flex space-x-4">
+                    <div className="rounded-full bg-slate-900 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="h-2 bg-slate-900 rounded"></div>
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="h-2 bg-slate-900 rounded col-span-2"></div>
+                                <div className="h-2 bg-slate-900 rounded col-span-1"></div>
+                            </div>
+                            <div className="h-2 bg-slate-900 rounded"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="border  shadow rounded-md p-4 max-w-sm w-full">
+                <div className="animate-pulse flex space-x-4">
+                    <div className="rounded-full bg-slate-900 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="h-2 bg-slate-900 rounded"></div>
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="h-2 bg-slate-900 rounded col-span-2"></div>
+                                <div className="h-2 bg-slate-900 rounded col-span-1"></div>
+                            </div>
+                            <div className="h-2 bg-slate-900 rounded"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="border  shadow rounded-md p-4 max-w-sm w-full">
+                <div className="animate-pulse flex space-x-4">
+                    <div className="rounded-full bg-slate-900 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="h-2 bg-slate-900 rounded"></div>
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="h-2 bg-slate-900 rounded col-span-2"></div>
+                                <div className="h-2 bg-slate-900 rounded col-span-1"></div>
+                            </div>
+                            <div className="h-2 bg-slate-900 rounded"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
