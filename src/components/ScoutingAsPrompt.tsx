431d26162cc7fe223e1325b43b6e5cd659fea6e5
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-08-11 19:07:16 +0200

    change style

--- src/components/ScoutingAsPrompt.tsx
+++ src/components/ScoutingAsPrompt.tsx
@@ -82,10 +82,11 @@ export default function Chat() {
 
 
   return (
-    <div className="w-full flex items-center justify-center ml-[20%]">
+    <div className="w-full flex items-center justify-center ">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-1/2 space-y-8 p-8 border-r-2">
-          <FormField
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-8 space-x-24 border-r-2 pr-6 flex">
+          <div className="flex flex-col w-min space-y-7 !mt-10">  
+            <FormField
             control={form.control}
             name="fineGrained"
             render={({ field }) => (
@@ -105,61 +106,62 @@ export default function Chat() {
               </FormItem>
             )}
           />
-          <FormField
-            control={form.control}
-            name="position"
-            render={({ field }) => (
-              <FormItem className="w-3/5 min-w-80">
-                <FormLabel>Position</FormLabel>
-                <FormDescription>
-                  Optional. Can limit the results because players that didn't play their main position won't be found.
-                </FormDescription>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormField
+              control={form.control}
+              name="position"
+              render={({ field }) => (
+                <FormItem className="w-3/5 min-w-80">
+                  <FormLabel>Position</FormLabel>
+                  <FormDescription>
+                    Optional. Can limit the results because players that didn't play their main position won't be found.
+                  </FormDescription>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a position to search for..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {positions}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            /></div>
+          <div className="flex flex-col w-full space-y-7 !mt-0">
+            <FormField
+              control={form.control}
+              name="prompt"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">Scouting Prompt</FormLabel>
                   <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a position to search for..." />
-                    </SelectTrigger>
+                    <Textarea placeholder="Player search prompt..." {...field} />
                   </FormControl>
-                  <SelectContent>
-                    {positions}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="prompt"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Scouting Prompt</FormLabel>
-                <FormControl>
-                  <Textarea placeholder="Player search prompt..." {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          {<PlayerList playerListToParent={setPlayerAndReports} playerList={playerList} query={form.getValues().prompt}></PlayerList>}
-          {!isLoading && <Button type="submit" >Submit</Button>}
-          {isLoading &&
-            <>
-              <Skeleton></Skeleton>
-              <Button type="button" disabled>
-                <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                Processing...
-              </Button>
-            </>
-          }
+            {<PlayerList playerListToParent={setPlayerAndReports} playerList={playerList} query={form.getValues().prompt}></PlayerList>}
+            {!isLoading && <Button className="w-min" type="submit" >Submit</Button>}
+            {isLoading &&
+              <>
+                <Skeleton></Skeleton>
+                <Button  className="w-min" type="button" disabled>
+                  <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  Processing...
+                </Button>
+              </>
+            }</div>
         </form>
       </Form>
-      <div className="p-8 w-1/2 self-start max-w-1/2 sticky top-0"> <OriginalReports reports={originalReports}/></div>
-     
+      <div className="p-8 w-1/3 self-start max-w-1/2 sticky top-0"> <OriginalReports reports={originalReports} /></div>
+
     </div>
 
   )
