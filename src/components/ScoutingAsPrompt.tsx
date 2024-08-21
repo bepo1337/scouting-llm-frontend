ef5b01b101d80bb4f0ecf53e06f945b49599bc9d
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-08-21 20:21:48 +0200

    fix buggy centering of components

--- src/components/ScoutingAsPrompt.tsx
+++ src/components/ScoutingAsPrompt.tsx
@@ -100,7 +100,7 @@ export default function Chat() {
 
 
   return (
-    <div className="w-full flex items-center justify-center ">
+    <div className="w-full flex justify-center ">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-8 border-r-2 pr-6 flex">
           <div className="flex flex-col w-min space-y-7 !mt-0 border-r-2 pr-10">  
